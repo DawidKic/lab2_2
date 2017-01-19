682a85b0367cc9c5bd56011dc5c5a69a4fff1ef0
Dawid Kicinski <d.kicinski12@gmail.com>
2017-01-19 17:20:49 +0100

    Dodanie testu funkcji multiplyBy

--- src/test/java/lab2_2/MoneyClassTest.java
+++ src/test/java/lab2_2/MoneyClassTest.java
@@ -54,6 +54,34 @@ public class MoneyClassTest {
 		assertThat(money.toString(), is(equalTo(expected)));
 	}
 	
+	@Test
+	public void moneyPositiveMultiplier() {
+		Money money = new Money(10);
+		String expected = "100,00 EUR";
+		assertThat(money.multiplyBy(10).toString(), is(equalTo(expected)));
+	}
+	
+	@Test
+	public void moneyNegativeMultiplier() {
+		Money money = new Money(10);
+		String expected = "-100,00 EUR";
+		assertThat(money.multiplyBy(-10).toString(), is(equalTo(expected)));
+	}
+	
+	@Test
+	public void moneyMaxDoubleMultiplierValue() {
+		Money money = new Money(10);
+		String expected = new BigDecimal((Double.MAX_VALUE)).toPlainString()+"0,00 EUR";
+		assertThat(money.multiplyBy(Double.MAX_VALUE).toString(), is(equalTo(expected)));
+	}
+	
+	@Test
+	public void moneyMinDoubleMultiplierValue() {
+		Money money = new Money(10);
+		String expected = new BigDecimal(-Double.MAX_VALUE).toPlainString()+"0,00 EUR";
+		assertThat(money.multiplyBy(-Double.MAX_VALUE).toString(), is(equalTo(expected)));
+	}
+	
 	
 	
 	
